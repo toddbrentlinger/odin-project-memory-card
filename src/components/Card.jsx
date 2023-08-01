0bd575d48879e554b4aa8daef3695884ca6ba1bd
toddbrentlinger <toddbrentlinger@gmail.com>
2023-08-01 09:50:11 -0700

    Fix issue with some cards not spinning on rerender
    Adds animation reference when card mounts and plays animation on every
    render using effects.
    Remove card spin animation from stylesheet since it's added on mount
    inside component instead.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useRef } from "react";
+
 function Card(props) {
     const { image, title } = props.card;
+    const elementRef = useRef(null);
+    const animationRef = useRef(null);
 
     const handleSelectCard = () => {
         props.handleSelectCard(props.card.id);
     };
 
+    useEffect(() => {
+        animationRef.current = elementRef.current.animate(
+            [{transform: 'rotateY(180deg)'}, {transform: 'rotateY(0deg)'}],
+            {
+                easing: 'ease-in-out',
+                duration: 1000,
+            }
+        );
+    }, []);
+
+    useEffect(() => {
+        console.log(`Card Renders: ${title}`);
+        animationRef.current.play();
+    });
+
     return (
         <div
+            ref={elementRef}
             className="memory-card" 
             tabIndex={0}
             onClick={handleSelectCard}
